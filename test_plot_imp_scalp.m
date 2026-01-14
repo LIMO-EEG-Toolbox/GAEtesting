f57719dbbda0e4fd181e3604dc2150488fb87748
AlbertLi12 <144032121+AlbertLi12@users.noreply.github.com>
2026-01-14 22:13:37 +0100

    some test results

--- test_plot_imp_scalp.m
+++ test_plot_imp_scalp.m
@@ -1,11 +1,9 @@
 % Set path
 curr_path = pwd();
-analysis_type = 'N170';
-chanlocs_path = fullfile(curr_path, 'limo_chanlocs.mat');
-maskingRes_path = fullfile(curr_path, analysis_type, 'Masking', 'Channel Masking');
-chan_drop_path = fullfile(maskingRes_path, 'imp_chan_drop.mat');
-chan_mean_path = fullfile(maskingRes_path, 'imp_chan_mean.mat');
-chan_zero_path = fullfile(maskingRes_path, 'imp_chan_zero.mat');
+analysis_type = 'P3';
+chanlocs_path = fullfile(curr_path, 'datain', 'limo_chanlocs.mat');
+maskingRes_path = fullfile(curr_path, analysis_type, 'Ablation', 'Channel Ablation');
+chan_mean_path = fullfile(maskingRes_path, 'imp_chan_drop.mat');
 
 % Create plot save folder
 plotSaveFolder = fullfile(maskingRes_path, 'Scalp Plots');
@@ -14,33 +12,29 @@ if ~exist(plotSaveFolder, 'dir')
 end
 
 % Load the adjacency matrix and data
-chan_drop = load(chan_drop_path).all_imp_chan_drop;
-chan_mean = load(chan_mean_path).all_imp_chan_mean;
-chan_zero = load(chan_zero_path).all_imp_chan_zero;
+chan_mean = load(chan_mean_path).all_imp_chan_drop;
 expected_chanlocs = load(chanlocs_path).expected_chanlocs;
 
 % Generate scalp plots for each beta
-[nBeta, nSubject, nChan] = size(chan_drop);
+[nBeta, nSubject, nChan] = size(chan_mean);
+
+chan_mean_norm = zeros(size(chan_mean));
+eps_val = 1e-12;
 
 for iBeta = 1:nBeta
-    val_drop = squeeze(mean(chan_drop(iBeta,:,:), 2));
-    title_drop = sprintf('Channel Importance Drop Method | beta #%d', iBeta);
-    filename_drop = sprintf('Imp_scalp_beta_%02d_drop.png', iBeta);
-    plot_chan_importance(val_drop, expected_chanlocs, ...
-                         title_drop, plotSaveFolder, filename_drop);
-
-    val_mean = squeeze(mean(chan_mean(iBeta,:,:), 2));
-    title_mean = sprintf('Channel Importance Mean Method | beta #%d', iBeta);
-    filename_mean = sprintf('Imp_scalp_beta_%02d_mean.png', iBeta);
-    plot_chan_importance(val_mean, expected_chanlocs, ...
-                         title_mean, plotSaveFolder, filename_mean);
-
-    val_zero = squeeze(mean(chan_zero(iBeta,:,:), 2));
-    title_zero = sprintf('Channel Importance Zero Method | beta #%d', iBeta);
-    filename_zero = sprintf('Imp_scalp_beta_%02d_zero.png', iBeta);
-    plot_chan_importance(val_zero, expected_chanlocs, ...
-                         title_zero, plotSaveFolder, filename_zero);
+    X = squeeze(chan_mean(iBeta,:,:));
+    denom = max(X(:));
+    denom = max(denom, eps_val);
+    chan_mean_norm(iBeta,:,:) = X ./ denom;
 end
+
+beta_chan_mean = squeeze(mean(chan_mean_norm, 2));
+final_chan_imp = mean(beta_chan_mean, 1)';
+filename  = sprintf('%s_Imp_scalp.png', analysis_type);
+title_str = sprintf('%s | Aggregated Channel Importance', analysis_type);
+fprintf('Saved scalp plot: %s\n', fullfile(plotSaveFolder, filename));
+
+plot_chan_importance(final_chan_imp, expected_chanlocs, title_str, plotSaveFolder, filename);
 
 function plot_chan_importance(val, expected_chanlocs, ...
                          title_str, plotSaveFolder, filename)
@@ -48,10 +42,12 @@ function plot_chan_importance(val, expected_chanlocs, ...
     val_centered = val - mean(val);
     c = max(abs(val_centered));
     figure('Color','w','NumberTitle','off','Name','limo_best_electrodes.m');
+    val_plot = val;
+    cmax = max(val_plot);
     opt = {'electrodes','on', ...
-           'maplimits',[-c c], ...
-           'verbose','off', ...
-           'colormap', flipud(limo_color_images(val_centered))};
+       'maplimits',[0 cmax], ...
+       'verbose','off', ...
+       'colormap', limo_color_images(val_plot)};
 
     topoplot(val_centered, expected_chanlocs, opt{:});
     %colorbar;
